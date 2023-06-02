94f42aaca8bbc2922cadf3603b424969861b6672
Will Allan <will.allan@disneystreaming.com>
2023-06-01 23:57:19 -0700

    Stuff

--- apps/docs/src/main.ts
+++ apps/docs/src/main.ts
@@ -2,19 +2,25 @@ import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import { Header, Counter, setupCounter } from "ui";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    ${Header({ title: "Docs" })}
-    <div class="card">
-      ${Counter()}
-    </div>
-  </div>
-`;
+const appElement = document.querySelector<HTMLDivElement>("#app");
+const counterElement = document.querySelector<HTMLButtonElement>("#counter");
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+if (appElement) {
+  appElement.innerHTML = `
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      </a>
+      ${Header({ title: "Docs" })}
+      <div class="card">
+        ${Counter()}
+      </div>
+    </div>`;
+}
+
+if (counterElement) {
+  setupCounter(counterElement);
+}
